24962158316586e29045c425e77c6e40f07a3025
Makise0721 <153273014+Makise0721@users.noreply.github.com>
2024-01-03 22:23:10 +0800

    Update 乘法口诀数列.c
    xdoj king

--- 乘法口诀数列.c
+++ 乘法口诀数列.c
@@ -1,36 +1,21 @@
 #include <stdio.h>
-
-int main(){
-	int a1, a2, n;
-	scanf("%d %d %d", &a1, &a2, &n);
-	
-	int cxk[3000] = {0};
-	cxk[0] = a1;
-	cxk[1] = a2;
-	
-	if ((a1 >= 01 && a1 <= 9) && (a2 >= 0 && a2 <= 9) && (n > 0 && n <= 1000)) {
-		if (n == 1) {
-			printf("%d", a1);
-		} else if (n == 2) {
-			printf("%d %d", a1, a2);
+int main() {
+	int m, n, a, b[100] = {0}, i, j, p;
+	scanf("%d %d %d", &m, &n, &a);
+	b[0] = m;
+	b[1] = n;
+	for (i = 2, j = 2; i < a; i++, j++) {
+		p = b[i - 2] * b[i - 1];
+		if (p < 10) {
+			b[j] = p;
 		} else {
-			int kunkun = 2;
-			for (int i = 2; i < n; i++) {
-				int next = cxk[i - 1] * cxk[i - 2];
-				if (next >= 10) {
-					cxk[kunkun + 1] = next % 10;
-					cxk[kunkun] = next / 10;
-					kunkun += 2;
-				} else {
-					cxk[kunkun++] = next;
-				}
-			}
-			
-			for (int i = 0; i < n; i++) {
-				printf("%d ", cxk[i]);
-			}
+			b[j + 1] = p % 10;
+			b[j] = p / 10;
+			j++;
 		}
 	}
-	
+	for (i = 0; i < a; i++) {
+		printf("%d ", b[i]);
+	}
 	return 0;
 }
